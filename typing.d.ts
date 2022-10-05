a89de9d25e3666c9956f9f05dd9a38447b0d4356
Mila <milashaohuibergmann@gmail.com>
2022-10-05 19:23:35 +0200

    interface done

--- typing.d.ts
+++ typing.d.ts
@@ -6,17 +6,59 @@ interface SanityBody {
 }
 
 interface Image {
-    _type:"image";
+    _type: "image";
     asset: {
-        _ref:string;
-        _type:"reference"
-    }
+        _ref: string;
+        _type: "reference";
+    };
 }
 
-
+export interface PageInfo extends SanityBody {
+    _type: "PageInfo";
+    address: string;
+    backgroundInfo: string;
+    email: string;
+    role: string;
+    heroImg: Image;
+    phoneNumber: string;
+    profilePic: Image;
+}
 
 export interface Social extends SanityBody {
     _type: "social";
     title: string;
     url: string;
+}
+
+export interface Skill extends SanityBody {
+    _type: "skill";
+    image: Image;
+    progress: number;
+    title: string;
+}
+export interface Technology extends SanityBody {
+    _type: "skill";
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Project extends SanityBody {
+    title: string;
+    _type: "project";
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technology: Technology[];
+}
+
+export interface Experience extends SanityBody {
+    _type: "experience";
+    compony: string;
+    companyImg: Image;
+    dateStarted: date;
+    dateEnded: date;
+    jobTitle: string;
+    points: string[];
+    technology: Technology[];
 }
